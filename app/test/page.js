b6d87e5fa0bb4d5623ca8479323026103a0d2268
sapioXmachina <vpbadia23@icloud.com>
2023-07-04 20:23:14 +0000

    Fixed test/page typo

--- app/test/page.js
+++ app/test/page.js
@@ -42,8 +42,6 @@ export default function Home() {
     }
   };
 
-  const notify = () => toast("Wow so easy !");
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
