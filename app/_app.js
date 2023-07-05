bd62691126aae6f870458e13737be1929d05bed2
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 09:20:59 +0000

    Updating nprogress

--- app/_app.js
+++ app/_app.js
@@ -1,15 +1,35 @@
 // app_.js
-import NProgress from "nprogress";
-import Router, { useRouter } from "next/router";
 
-Router.onRouteChangeStart = url => { NProgress.start() };
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
+import NProgress from 'nprogress';
+import "nprogress/nprogress.css";
+import Router from 'next/router';
 
-function MyApp({ Component, pageProps }) {
-  const {route} = useRouter();
-  return (
-    <Component {...pageProps} key={route} />
-  );
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+export default function () {
+    return null;
 }
-export default MyApp;
+
+// import NProgress from "nprogress";
+// import Router, { useRouter } from "next/router";
+
+// Router.onRouteChangeStart = url => { NProgress.start() };
+// Router.onRouteChangeComplete = () => NProgress.done();
+// Router.onRouteChangeError = () => NProgress.done();
+
+// function MyApp({ Component, pageProps }) {
+//   const {route} = useRouter();
+//   return (
+//     <Component {...pageProps} key={route} />
+//   );
+// }
+// export default MyApp;
