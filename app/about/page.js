64ad85a4b98ef8229ba79bf174f02541b5629620
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 21:01:23 +0000

    Updated github spacing

--- app/about/page.js
+++ app/about/page.js
@@ -67,7 +67,7 @@ export default function Home() {
 
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <h1>{process.env.NEXT_PUBLIC_APP_TITLE}</h1>
-          &nbsp;| Github :&nbsp;
+          &nbsp;| Github:&nbsp;
           <Link target="_blank" href={process.env.NEXT_PUBLIC_GIT_URL}>
             <code className="font-mono font-bold">{process.env.NEXT_PUBLIC_GIT_HUB}</code>
           </Link>
