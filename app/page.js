f96dff5e301aba368549e514ca3b7599e9a5c5cf
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 17:44:34 +0000

    Fixing app_vars

--- app/page.js
+++ app/page.js
@@ -65,8 +65,8 @@ export default function Home() {
           </>
         </div>
 
-        <p>{process.env.NEXT_PUBLIC_APP_NAME}</p>
-        <p>{process.env.NEXT_PUBLIC_APP_PROJ}</p>
+        <h1>{process.env.NEXT_PUBLIC_APP_NAME}</h1>
+        <h2>{process.env.NEXT_PUBLIC_APP_PROJ}</h2>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Github :&nbsp;
           <Link target="_blank" href={process.env.NEXT_PUBLIC_GIT_URL}>
