6088cfd25c51fd81a69a78bea002c43c7357754e
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 02:03:45 +0000

    Fixing nprogress error

--- app/_app.js
+++ app/_app.js
@@ -1,6 +1,6 @@
 // _app.js
 import { useState , useEffect} from 'react';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
 
 //Route Events
@@ -9,20 +9,10 @@ Router.events.on('routeChangeComplete', () => NProgress.set(1.0));
 Router.events.on('routeChangeError', () => NProgress.set(1.0));
 
 function MyApp({ Component, pageProps }) {
-  /**@type {[{target:HTMLDivElement},Dispatch<setStateAction<{target:HTMLDivElement}>>]}*/
-  const [option,setOption] = useState({target:null})
+  const {route} = useRouter();
 
-  useEffect(() => {
-    setOption({
-      target:window.document.getElementById("__next" )
-    });
-  }, []);
-
-  Router.events.on('routeChangeStart', () => NProgress.set(0.3));
-  Router.events.on('routeChangeComplete', () => NProgress.set(1.0));
-  Router.events.on('routeChangeError', () => NProgress.set(1.0));
-
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} key={route} />
+  );
 }
-
 export default MyApp;
