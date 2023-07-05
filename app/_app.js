dafb32944e27d044cef1bbe7e9b31f688188c2a6
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 08:00:38 +0000

    Reverting to loading-1.4

--- app/_app.js
+++ app/_app.js
@@ -1,45 +1,17 @@
+// _app.js
 import Router, { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-
-import Loader from './loader';
 import NProgress from 'nprogress';
 
+//Route Events
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
-  const [isLoading] = useState(false);
-  const {router} = useRouter();
-
-  useEffect(() => {
-    Router.events.on("routeChangeStart", (url)=>{
-      NProgress.start();
-    });
-    Router.events.on("routeChangeComplete", (url)=>{
-      NProgress.done(false);
-    });
-
-    Router.events.on("routeChangeError", (url) =>{
-      NProgress.done(false);
-    });
-  }, []);
+  const {route} = useRouter();
 
   return (
-    <>
-      {isLoading && <Loader/>}
-      <Component {...pageProps} key={router} />
-    </>
-  )
+    <Component {...pageProps} key={route} />
+  );
 }
-
-export default MyApp
-
-// import Router from 'next/router';
-// import NProgress from 'nprogress'; //nprogress module
-
-// //Route Events
-// Router.events.on('routeChangeStart', () => NProgress.start());
-// Router.events.on('routeChangeComplete', () => NProgress.done());
-// Router.events.on('routeChangeError', () => NProgress.done());
-
-// function MyApp({ Component, pageProps }) {
-//     return <Component {...pageProps} />
-// }
-// export default MyApp;
+export default MyApp;
