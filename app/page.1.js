49704c6ed8339c0330c33d1f4ac180cbb166b181
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 05:47:48 +0000

    Fixing nprogress document error

--- app/page.1.js
+++ app/page.1.js
@@ -8,6 +8,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import NProgress from 'nprogress'; //nprogress module
 
 export default function Home() {
+  NProgress.start();
+  NProgress.done();
+  
   const [message, setMessage] = useState("");
   const discordMessage = process.env.NEXT_PUBLIC_API_MSG + " ";
 
