2c391223898865682722a6c42710b52254c2a404
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 11:05:41 +0000

    Updated _app

--- app/_app.js
+++ app/_app.js
@@ -2,9 +2,9 @@
 import Router from 'next/router';
 import NProgress from './nprogress.js';
 
-Router.events.on('routeChangeStart', NProgress.start);
-Router.events.on('routeChangeError', NProgress.done);
-Router.events.on('routeChangeComplete', NProgress.done);
+Router.events.on('routeChangeStart', NProgress.start());
+Router.events.on('routeChangeError', NProgress.done());
+Router.events.on('routeChangeComplete', NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
