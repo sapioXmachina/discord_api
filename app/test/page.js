2f141d7a491db34fc27c68c72fe70eb5967729c9
sapioXmachina <vpbadia23@icloud.com>
2023-07-05 06:52:12 +0000

    Commented out nprogress on about/test

--- app/test/page.js
+++ app/test/page.js
@@ -5,13 +5,11 @@ import Link from 'next/link';
 
 import { ToastContainer, toast } from 'react-toastify';
 
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css'; //styles of nprogress
+// import NProgress from 'nprogress';
 
 export default function Home() {
-  NProgress.start();
-  NProgress.done();
+  // NProgress.start();
+  // NProgress.done();
   
   const [message, setMessage] = useState("");
   const discordMessage = process.env.NEXT_PUBLIC_API_MSG + " ";
