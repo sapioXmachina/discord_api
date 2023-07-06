86d456221a7a34936c5d2a4b9da0646bea22949c
sapioXmachina <vpbadia23@icloud.com>
2023-07-06 17:14:51 +0000

    Updated pages, no import_bootstrap

--- app/nprogress/page.js
+++ app/nprogress/page.js
@@ -2,9 +2,6 @@
 "use client";
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import 'bootstrap/dist/css/bootstrap.css'; //styles of bootstrap
-
-import { ToastContainer, toast } from 'react-toastify';
 
 import NProgress from 'nprogress';
 
@@ -45,8 +42,8 @@ export default function Page() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col bg-dark-subtle align-items-center text-center justify-between">
-      <nav className="fixed-top p-2 bg-dark text-center">
+    <div className="flex min-h-screen flex-col bg-secondary align-items-center text-center justify-between">
+      <nav className="sticky-top w-100 p-2 bg-dark text-center">
         <span className="text-light">
           NProgress Bar & Spinner
         </span>
@@ -61,38 +58,44 @@ export default function Page() {
           </Link>
         </div>
       </nav>
-      <div className="vh-100 d-flex mt-14 pt-14">
-        <form>
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="discord"
-              name="discord"
-              aria-describedby="discordHelp"
-              style={{height: 132, paddingBottom: 32 + "%"}}
-              size="32"
-              minLength="1"
-              maxLength="140"
-              placeholder="Enter a message..."
-              pattern="^[\w\d\s\S\D\W]{1,140}"
-              title="Should be only letters or numbers."
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            />
-            <div id="discordHelp" className="form-text text-body-seondary">
-              Only letters and spaces allowed.
+      <div className="vh-100 d-flex justify-content-center align-items-start">
+        <div className="card ps-3 pe-3 pt-2 pb-3 mt-3 bg-dark-subtle">
+          <p
+            className="fs-1 fw-bold text-left pt-4"
+            style={{lineHeight: 0}}
+            >Homework</p>
+          <hr className="hr" />
+          <form>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="discord"
+                name="discord"
+                aria-describedby="discordHelp"
+                style={{height: 132, paddingBottom: 32 + "%"}}
+                size="32"
+                minLength="1"
+                maxLength="140"
+                placeholder="Enter a message..."
+                pattern="^[\w\d\s\S\D\W]{1,140}"
+                title="Should be only letters or numbers."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+              <div id="discordHelp" className="form-text text-body-seondary">
+                Only letters and spaces allowed.
+              </div>
             </div>
-          </div>
-          <button
-            className="btn btn-secondary"
-            type="submit"
-            onClick={handleSubmit}
-            >Send Message
-          </button>
-          <ToastContainer />
-        </form>
+            <button
+              className="btn btn-secondary"
+              type="submit"
+              onClick={handleSubmit}
+              >Send Message
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
