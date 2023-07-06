9b8706e7cef07730146f74a18369ed0ad738f980
sapioXmachina <vpbadia23@icloud.com>
2023-07-06 17:13:23 +0000

    Final working fix for bootstrap_css_sourcemap; Moved import_toast_container to _app

--- app/_app.js
+++ app/_app.js
@@ -1,13 +1,21 @@
 // _app.js
 import Router from 'next/router';
-import NProgress from './nprogress.js';
+import NProgress from 'nprogress';
+
+import { useEffect } from 'react';
 
 Router.events.on('routeChangeStart', NProgress.start());
 Router.events.on('routeChangeError', NProgress.done());
 Router.events.on('routeChangeComplete', NProgress.done());
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (<>
+  <Component {...pageProps} />
+  <ToastContainer />
+  </>);
 }
 
 export default MyApp;
--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,24 @@
 // layout.js
 import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.css'; //styles of bootstrap
 import './globals.css';
-// import './nprogress.css'; //custom styles of nprogress
 import 'nprogress/nprogress.css'; //styles of nprogress
-import 'bootstrap/dist/css/bootstrap.css'; //stules of bootstrap
-import 'react-toastify/dist/ReactToastify.css'; //styles of toastify
+
 import { Inter } from 'next/font/google';
-
 const inter = Inter({ subsets: ['latin'] });
-
+ 
 export const metadata = {
+  charset: 'utf-8',
+  name: 'viewport',
+  content: 'width=device-width, initial-scale=1',
   title: 'Discord API | HackerCoop | Summer 2023',
   description: 'Created by sapioXmachina@github on Vercel',
-};
+ };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
         <body className={inter.className}>{children}</body>
