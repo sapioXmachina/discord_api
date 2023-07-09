82cc8891a0701b8311ee944a2b6b3065aa2b2498
sapioXmachina <vpbadia23@icloud.com>
2023-07-09 00:21:36 +0000

    Commented out toastify _app

--- app/_app.js
+++ app/_app.js
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
+// import 'react-toastify/dist/ReactToastify.css';
 // import { ToastContainer, toast } from 'react-toastify';
 
 Router.events.on('routeChangeStart', NProgress.start());
