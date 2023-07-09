c5f73208abb0d8616546171d421c57b1caeb0ad5
sapioXmachina <vpbadia23@icloud.com>
2023-07-09 09:47:11 +0000

    Updated all discordservices with env_vars

--- app/DiscordService.js
+++ app/DiscordService.js
@@ -17,7 +17,7 @@ function DiscordService(setFormData) {
       NProgress.start();
       
       const data = await axios.post(
-        "https://discord.com/api/webhooks/1126411932103086120/8NKth6WdnNanjeflYKR4ITZpildJWogLYX9VEinSdJbMcNpegt3I_6XMUZBaSzI7VJys",
+        process.NEXT_PUBLIC_API_SAPIO,
         body
       );
       setFormData(initialFormState);
